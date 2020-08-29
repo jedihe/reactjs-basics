99388be1ec720ca4f591947f4c7b896862575884
John Herreño <jedihe@gmail.com>
2020-08-29 16:31:24 -0500

    Define App component, render simple markup, use bootstrap css for basic layout

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-10 col-xs-offset-1">
+            <h1>Hello!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, window.document.getElementById('app'));
